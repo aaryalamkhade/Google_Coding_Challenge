fe6297c68dab50070904f8499ca33177365882f1
Aarya <49730497+aaryalamkhade@users.noreply.github.com>
2020-08-17 15:18:59 +0530

    Add files via upload

---
+++ Google string code.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string s="aacbadfeefff";
+	int length=s.length();
+	int count=0;
+	for(int i=1;i<length;i++)
+	{   if(s[i]>=s[i-1])
+	{
+		while(s[i]>=s[i-1] && i<length)
+		{
+			i++;
+		}
+		count++;
+	}
+	else
+	{
+		while(s[i]<=s[i-1] && i<length)
+       {
+          i++;	
+	   }
+	   count++;
+		}	
+		
+	   		
+	}
+	cout<<"String:"<<s<<endl<<"Good_string_count:"<<count;
+}
